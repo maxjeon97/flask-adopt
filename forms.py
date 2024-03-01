6573df80216812e93336ad482a6f4c7502892645
Max Jeon <maxjeon97@gmail.com>
2024-03-01 12:19:42 -0800

    Work on Step 3

--- forms.py
+++ forms.py
@@ -1,1 +1,20 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, FloatField, IntegerField, BooleanField
+from wtforms import DateTimeField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Optional
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets"""
+
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = StringField("Pet Species", validators=[InputRequired()])
+    photo_url = StringField("Picture of Pet")
+    age = SelectField("Age",
+        choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'),
+                 ('senior', 'Senior')],
+        validators=[InputRequired()]
+    )
+    notes = TextAreaField("Notes about Pet")
--- models.py
+++ models.py
@@ -14,3 +14,45 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    """Pets. Adoption site has many pets"""
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    species = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    photo_url = db.Column(
+        db.String(100),
+        nullable=False,
+        default=""
+    )
+
+    age = db.Column(
+        db.String(6),
+        nullable=False
+    )
+
+    notes = db.Column(
+        db.Text,
+        default=""
+    )
+
+    available = db.Column(
+        db.Boolean,
+        default=True
+    )
