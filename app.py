91c947b7e16e24881ad0de280fd6d2088631f948
Emily Walker <ewalker415@gmail.com>
2024-03-01 15:00:18 -0800

    added add_pet form

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 from flask import Flask, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import connect_db, Pet
+from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -27,3 +28,26 @@ toolbar = DebugToolbarExtension(app)
 def show_homepage():
     pets = Pet.query.all()
     return render_template('index.html', pets=pets)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    """ Show add pet form and handle adding pet"""
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        pet = Pet(name = name, species=species,
+                  photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f'Put up {name} for adoption!')
+        return redirect('/')
+    else:
+        return render_template("add_pet_form.html",form=form)
--- forms.py
+++ forms.py
@@ -3,15 +3,17 @@ from flask_wtf import FlaskForm
 
 from wtforms import StringField, FloatField, IntegerField, BooleanField
 from wtforms import DateTimeField, SelectField, TextAreaField
-from wtforms.validators import InputRequired, Optional
+from wtforms.validators import InputRequired, Optional, URL
 
 
 class AddPetForm(FlaskForm):
     """Form for adding pets"""
 
     name = StringField("Pet Name", validators=[InputRequired()])
-    species = StringField("Pet Species", validators=[InputRequired()])
-    photo_url = StringField("Picture of Pet")
+    species = SelectField("Pet Species",
+        choices=[('cat', 'Cat'), ('dog', 'Dog'),('porcupine','Porcupine')],
+        validators=[InputRequired()])
+    photo_url = StringField("Picture of Pet", validators=[URL(), Optional()])
     age = SelectField("Age",
         choices=[('baby', 'Baby'), ('young', 'Young'), ('adult', 'Adult'),
                  ('senior', 'Senior')],
